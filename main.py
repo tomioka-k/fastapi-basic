538ae2e1de51c694bb8465d541aec1cdad3ed3ed
tomioka-k <tomioka-k@tajima.co.jp>
2021-12-11 01:56:15 +0900

    parameter-body

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List, Dict
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return 'Hello World'
+
+class ArticleModel(BaseModel):
+    title: str
+    content: str
+    username: str
+    tag: List[str] = []
+    metadata: Dict[str, str] = {'key': 'value'}
+
+@app.post('/article')
+def get_article(article: List[ArticleModel], username: Optional[str] = None, is_display: bool = True):
+    return {
+        'article': article,
+        'username': username,
+        'is_display': is_display
+    }
+
+@app.get('/article/all')
+def get_article(username: Optional[str] = None, is_display: bool = True):
+    return {'message': f'article all query {username} and {is_display}'}
+
+@app.get('/article/{id}')
+def get_article(id: int): 
+    return {'message': f'article is {id}'}
+
+@app.get('/article/{id}/comments/{comment_id}')
+def get_article(id: int, comment_id: int):
+    return {'message': f'article is {id} and {comment_id}'}
